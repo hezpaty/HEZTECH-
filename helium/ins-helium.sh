786641df1ddf3018b91b086fa07e72ad09d562b1
hezpaty <hezpaty07@gmail.com>
2025-12-09 22:35:09 +0300

    Hezpaty

--- helium/ins-helium.sh
+++ helium/ins-helium.sh
@@ -2,9 +2,9 @@
 # =========================================
 # Quick Setup | Script Setup Manager
 # Edition : Stable Edition 1.0
-# Auther  : NETWORK TWEAKER
+# Auther  : HEZPATY
 # Kuota Habisss
-# (C) Copyright 2023
+# (C) Copyright 2025
 # =========================================
 # pewarna hidup
 BGreen='\e[1;32m'
@@ -12,15 +12,15 @@ NC='\e[0m'
 clear
 rm -rf /usr/bin/m-system
 echo -e "\e[1;32m Update Menu System.. \e[0m"
-wget -q -O /usr/bin/m-system https://raw.githubusercontent.com/Hubdarkweb/Lenin/master/helium/menu/m-system.sh
+wget -q -O /usr/bin/m-system https://raw.githubusercontent.com/hezpaty/HEZTECH-/main/helium/menu/m-system.sh
 chmod +x /usr/bin/m-system
 rm -rf /usr/bin/wbmn
 echo -e "\e[1;32m Start download Panel Webmin.. \e[0m"
-wget -q -O /usr/bin/wbmn https://raw.githubusercontent.com/Hubdarkweb/Lenin/master/helium/webmin/wbmn.sh
+wget -q -O /usr/bin/wbmn https://raw.githubusercontent.com/hezpaty/HEZTECH-/main/helium/webmin/wbmn.sh
 chmod +x /usr/bin/wbmn
 rm -rf /usr/bin/helium
 echo -e "\e[1;32m Start download Panel ADS Block.. \e[0m"
-wget -q -O /usr/bin/helium https://raw.githubusercontent.com/Hubdarkweb/Lenin/master/helium/helium.sh
+wget -q -O /usr/bin/helium https://raw.githubusercontent.com/hezpaty/HEZTECH-/main/helium/helium.sh
 chmod +x /usr/bin/helium
 echo -e "\e[1;32m Setup done Please wait.. \e[0m"
 sleep 2
