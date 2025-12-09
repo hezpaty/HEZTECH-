f093fcff766ea2ca1f09d66b5f32ff0e90b01356
hezpaty <hezpaty07@gmail.com>
2025-12-10 01:35:53 +0300

    Hezpaty

--- ubuntu/setup.sh
+++ ubuntu/setup.sh
@@ -18,7 +18,7 @@ yellow() { echo -e "\\033[33;1m${*}\\033[0m"; }
 green() { echo -e "\\033[32;1m${*}\\033[0m"; }
 red() { echo -e "\\033[31;1m${*}\\033[0m"; }
 # domain random
-CDN="https://raw.githubusercontent.com/Hubdarkweb/Lenin/master/ssh"
+CDN="https://raw.githubusercontent.com/hezpaty/HEZTECH-/main/ssh"
 cd /root
 #System version number
 if [ "${EUID}" -ne 0 ]; then
@@ -115,19 +115,19 @@ echo ""
 clear
     echo -e "$BBlue                    NT SETUP DOMAIN VPS     $NC"
     echo -e "$BYellow----------------------------------------------------------$NC"
-    echo -e "$BGreen 1. Use Domain Random / Gunakan Domain Random $NC"
-    echo -e "$BGreen 2. Choose Your Own Domain / Gunakan Domain Sendiri $NC"
+    echo -e "$BGreen 1. Use Random Domain $NC"
+    echo -e "$BGreen 2. Choose Your Own Domain $NC"
     echo -e "$BYellow----------------------------------------------------------$NC"
-    read -rp " input 1 or 2 / pilih 1 atau 2 : " dns
+    read -rp " input 1 or 2 : " dns
 	if test $dns -eq 1; then
     clear
     apt install jq curl -y
     wget -q -O /root/cf "${CDN}/cf" >/dev/null 2>&1
     chmod +x /root/cf
     bash /root/cf | tee /root/install.log
-    print_success "Domain Random Done"
+    print_success "Random Domain Done"
 	elif test $dns -eq 2; then
-    read -rp "Enter Your Domain / masukan domain : " dom
+    read -rp "Enter Your Domain : " dom
     echo "IP=$dom" > /var/lib/ipvps.conf
     echo "$dom" > /root/scdomain
 	echo "$dom" > /etc/xray/scdomain
@@ -148,15 +148,15 @@ echo -e "$BGreen      Install NT SSH Websocket           $NC"
 echo -e "\e[33m-----------------------------------\033[0m"
 sleep 0.5
 clear
-wget https://raw.githubusercontent.com/Hubdarkweb/Lenin/master/ssh/ssh-vpn.sh && chmod +x ssh-vpn.sh && ./ssh-vpn.sh
+wget https://raw.githubusercontent.com/hezpaty/HEZTECH-/ssh/ssh-vpn.sh && chmod +x ssh-vpn.sh && ./ssh-vpn.sh
 #Instal Xray
 echo -e "\e[33m-----------------------------------\033[0m"
 echo -e "$BGreen          Install NT XRAY              $NC"
 echo -e "\e[33m-----------------------------------\033[0m"
 sleep 0.5
 clear
-wget https://raw.githubusercontent.com/Hubdarkweb/Lenin/master/xray/ins-xray.sh && chmod +x ins-xray.sh && ./ins-xray.sh
-wget https://raw.githubusercontent.com/Hubdarkweb/Lenin/master/sshws/insshws.sh && chmod +x insshws.sh && ./insshws.sh
+wget https://raw.githubusercontent.com/hezpaty/HEZTECH-/xray/ins-xray.sh && chmod +x ins-xray.sh && ./ins-xray.sh
+wget https://raw.githubusercontent.com/hezpaty/HEZTECH-/sshws/insshws.sh && chmod +x insshws.sh && ./insshws.sh
 clear
 cat> /root/.profile << END
 # ~/.profile: executed by Bourne-compatible login shells.
@@ -195,7 +195,7 @@ if [ ! -f "/etc/log-create-shadowsocks.log" ]; then
 echo "Log Shadowsocks Account " > /etc/log-create-shadowsocks.log
 fi
 history -c
-serverV=$( curl -sS https://raw.githubusercontent.com/Hubdarkweb/Lenin/master/menu/versi  )
+serverV=$( curl -sS https://raw.githubusercontent.com/hezpaty/HEZTECH-/main/menu/versi  )
 echo $serverV > /opt/.ver
 aureb=$(cat /home/re_otm)
 b=11
