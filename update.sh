07f0dc1b444b4eaa819a02789de221095ee620e4
hezpaty <hezpaty07@gmail.com>
2025-12-10 10:02:46 +0300

    Hezpaty

--- update.sh
+++ update.sh
@@ -58,7 +58,7 @@ res1() {
 netfilter-persistent
 clear
 echo -e "\033[36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e " \e[1m          UPDATED SCRIPT POWERED BY 🗽📡🛰️⛷️🤿🎯💻 HEZPATY Cyberpunk    \e[0m"
+echo -e " \e[1m          UPDATED SCRIPT POWERED BY 🗽📡🛰️⛷️🤿🎯💻 HEZTECH Cyberpunk    \e[0m"
 echo -e "\033[36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e ""
 echo -e "  \033[m update script service\033[m"
